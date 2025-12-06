59b1dd01d9d36087c5788d227e634050ff829e93
Meagan Waller <meagan@meaganwaller.com>
2025-12-06 13:07:18 -0500

    refactor: streamline API URL configuration by consolidating logic into apiUrl function

--- config/index.ts
+++ config/index.ts
@@ -1,12 +1,22 @@
 export const isStaging = process.env.NEXT_PUBLIC_APP_ENV === 'staging'
 export const isDevelopment = process.env.NEXT_PUBLIC_APP_ENV === 'development'
 export const isProduction = process.env.NEXT_PUBLIC_APP_ENV === 'production'
+export const isCI = !!process.env.CI
+
+const apiUrl = function () {
+  if (isStaging) {
+    return process.env.NEXT_PUBLIC_STAGING_GRAPHQL_API
+  } else if (isCI || isProduction) {
+    return process.env.NEXT_PUBLIC_PRODUCTION_GRAPHQL_API
+  } else {
+    return process.env.NEXT_PUBLIC_WORDPRESS_GRAPHQL_URL
+  }
+}
+
 
 export const CONFIG = {
   api: {
-    url: isStaging
-      ? process.env.NEXT_PUBLIC_STAGING_GRAPHQL_API
-      : process.env.NEXT_PUBLIC_WORDPRESS_GRAPHQL_URL,
+    url: apiUrl()
   },
   siteName: "Garbage Collector's Daughter",
   url: process.env.NEXT_PUBLIC_SITE_URL || 'http://localhost:3000',
